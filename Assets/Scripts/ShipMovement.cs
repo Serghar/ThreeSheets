9dc6ea5c37427c50cf594c63645a112838c68f11
Dylan Sharkey <Serghar12@gmail.com>
2017-01-08 16:38:57 -0800

    Almost nothing implemented yet, Initial Commit for collab

---
+++ Assets/Scripts/ShipMovement.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipMovement : MonoBehaviour {
+
+    public float sailSpeed = 1;
+    public float rowSpeed = 1;
+    public float turnSpeed = 1f; //degrees per frame update
+    public float rotSpeed; //Velocity modified turn speed
+
+    //Zones: Run, Beam, Broad, Close
+    public float[] mods = new float[4] { 0.75f, 1f, 0.5f, 0.25f };
+
+    private Rigidbody rb;
+    private bool sailsUp;
+
+	// Use this for initialization
+	void Start () {
+        rb = transform.GetComponent<Rigidbody>();
+        sailsUp = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            sailsUp = !sailsUp;
+        }
+        if (!sailsUp)
+        {
+            if (Input.GetKey("w")) {
+                rb.AddForce(transform.forward * rowSpeed);
+            }
+            rotSpeed = turnSpeed;
+        }
+
+        //Turning
+        if (Input.GetKey("d")) {
+            Quaternion q = Quaternion.AngleAxis(rotSpeed, transform.up) * transform.rotation;
+            rb.MoveRotation(q);
+            float mag = rb.velocity.magnitude;
+            rb.velocity = transform.forward * mag * 0.995f;
+        }
+        if (Input.GetKey("a")) {
+            Quaternion q = Quaternion.AngleAxis(-rotSpeed, transform.up) * transform.rotation;
+            rb.MoveRotation(q);
+            float mag = rb.velocity.magnitude;
+            rb.velocity = transform.forward * mag * 0.995f;
+        } 
+    }
+
+    public void WindMovement(float power, float range)
+    {
+        int zone = 3;
+        if(range < 30) {
+            zone = 0;
+        } else if (range < 75) {
+            zone = 1;
+        } else if (range < 105) {
+            zone = 2;
+        }
+        //REFACTOR: change to based on current velocity rather than current % force
+        rotSpeed = (Mathf.Abs(mods[zone] - 0.5f) * 2 + 1) * turnSpeed;
+
+        float force = power * sailSpeed * mods[zone];
+        rb.AddForce(transform.forward * force);
+    }
+
+    public bool GetSailStatus()
+    {
+        return sailsUp;
+    }
+}
---
+++ Assets/Scripts/WindScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindScript : MonoBehaviour {
+    public float windStrength;
+
+    //Degrees of effect of wind zone in terms of it's current forward angle
+    private float effRange = 150f;
+
+	void OnTriggerStay(Collider obj)
+    {
+        if(obj.tag == "Player")
+        {
+            //Check to see if sails are up
+            if (obj.GetComponent<ShipMovement>().GetSailStatus())
+            {
+                //Calculate the different in the ship angle and the windzone
+                float angleDiff = Mathf.Abs(Mathf.Abs(transform.eulerAngles.y - obj.transform.eulerAngles.y + 180) % 360 - 180);
+                if(angleDiff < effRange)
+                {
+                    obj.GetComponent<ShipMovement>().WindMovement(windStrength, angleDiff);
+                }
+            }
+        }
+    }
+}
